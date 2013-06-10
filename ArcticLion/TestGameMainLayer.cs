eb7d0bd398ab216d8f1b8bee32ac40eed1e234e0
Gabriel Lumbi <gabriel.lumbi@gmail.com>
2013-06-10 17:20:08 +0900

    Made the enemy test ship shoot projectiles. Handled multiple enemy ships in a collection.

--- ArcticLion/TestGameMainLayer.cs
+++ ArcticLion/TestGameMainLayer.cs
@@ -10,15 +10,21 @@ namespace ArcticLion
 	public class TestGameMainLayer : Layer
 	{
 		Ship ship;
-		EnemyShip enemyShip;
+		List<EnemyShip> enemyShips;
+		private double enemyShipFireDelay = 1d;
+//TODO: Remove
 		Camera2D camera;
-		Queue<Bullet> bullets; //TODO: change to List?
+		Queue<Bullet> bullets;
+//TODO: change to List?
+		Queue<Bullet> enemyBullets;
 
 		public TestGameMainLayer (int z, Ship ship, Camera2D camera) : base(z)
 		{
 			this.ship = ship;
 			this.camera = camera;
 			this.Add (ship);
+
+			enemyShips = new List<EnemyShip> ();
 
 			bullets = new Queue<Bullet> ();
 			for (int i=0; i<20; i++) {
@@ -27,8 +33,17 @@ namespace ArcticLion
 				Add (b);
 			}
 
-			enemyShip = new TestGameEnemyShip ();
-			Add (enemyShip);
+			enemyBullets = new Queue<Bullet> ();
+			for (int i=0; i<20; i++) {
+				Bullet b = new Bullet ();
+				enemyBullets.Enqueue (b);
+				Add (b);
+			}
+
+			EnemyShip testEnemy = new TestGameEnemyShip ();
+
+			enemyShips.Add (testEnemy);
+			Add (testEnemy);
 		}
 
 		public override void Update (GameTime gameTime)
@@ -42,13 +57,18 @@ namespace ArcticLion
 			double rotationAngle = Math.Atan2 ((mousePositionWorld.Y - ship.Position.Y),
 			                                   (mousePositionWorld.X - ship.Position.X));
 
-//			rotationAngle += Math.PI / 2;
-
 			ship.Rotation = rotationAngle;
 
-			foreach(Bullet b in bullets){
+			foreach (Bullet b in bullets) {
 				//TODO: get the bounds?
-				if(b.IsAlive && !camera.IsInView(b.Position, new Rectangle(0,0,8,8))){
+				if (b.IsAlive && !camera.IsInView (b.Position, new Rectangle (0, 0, 8, 8))) {
+					b.IsAlive = false;
+				}
+			}
+
+			foreach (Bullet b in enemyBullets) {
+				//TODO: get the bounds?
+				if (b.IsAlive && !camera.IsInView (b.Position, new Rectangle (0, 0, 8, 8))) {
 					b.IsAlive = false;
 				}
 			}
@@ -57,7 +77,7 @@ namespace ArcticLion
 			if (ms.LeftButton == ButtonState.Pressed) {
 				if (!bullets.Peek ().IsAlive) {
 					Bullet newBullet = bullets.Dequeue ();
-					Vector2 newBulletVelocity = Vector2.Normalize(mousePositionWorld - ship.Position);
+					Vector2 newBulletVelocity = Vector2.Normalize (mousePositionWorld - ship.Position);
 					newBulletVelocity *= 1500f;
 					newBulletVelocity += ship.Velocity;
 					Vector2 shipYaw = new Vector2 ((float)Math.Cos (ship.Rotation), 
@@ -66,36 +86,61 @@ namespace ArcticLion
 					bullets.Enqueue (newBullet);
 				}
 			}
-		}
 
-		public void DetectCollisions(){
-			foreach(Bullet b in bullets){
-				if (b.IsAlive && /*TODO: Loop thru enemies*/ enemyShip.Parent != null &&
-				    (b.Position - enemyShip.Position).Length() < 40f){
-					b.IsAlive = false;
-
-					//TODO: REMOVE!! FOR TESTING PURPOSES
-					List<EnemyShip> newEnemies = enemyShip.DestroyPart (enemyShip.Parts[1]);
-					if(newEnemies != null){
-						foreach (EnemyShip newEnemyShip in newEnemies) {
-							Add (newEnemyShip);
-						}
-
-						newEnemies [0].direction = -1;
-						newEnemies [1].direction = 1;
-
-						//TODO: Don't kill me bro!
-						enemyShip.Kill ();
+			//TODO: move shooting logic to enemy AI?
+			enemyShipFireDelay -= gameTime.ElapsedGameTime.TotalSeconds;
+			if (enemyShipFireDelay <= 0) {
+				foreach (EnemyShip es in enemyShips) {
+					if (!enemyBullets.Peek ().IsAlive) {
+						Bullet newEnemyBullet = enemyBullets.Dequeue ();
+						Vector2 newBulletVelocity = Vector2.Normalize (ship.Position - es.Position);
+						newBulletVelocity *= 500f;
+						newBulletVelocity += es.Velocity;
+						newEnemyBullet.Shoot (es.Position, newBulletVelocity);
+						enemyBullets.Enqueue (newEnemyBullet);
+						enemyShipFireDelay = 1d;
 					}
 				}
 			}
 		}
 
-		private Vector2 GetMousePositionWorld()
+		public void DetectCollisions ()
+		{
+			foreach (Bullet b in bullets) {
+				List<EnemyShip> enemyShipsCopy = new List<EnemyShip> ();
+				enemyShipsCopy.AddRange (enemyShips);
+				foreach (EnemyShip es in enemyShipsCopy) {
+					if (b.IsAlive && /*TODO: Loop thru enemies*/ es.Parent != null &&
+						(b.Position - es.Position).Length () < 30f) {
+						b.IsAlive = false;
+
+						//TODO: REMOVE!! FOR TESTING PURPOSES
+						if (es.Parts.Count > 1) {
+							List<EnemyShip> newEnemies = es.DestroyPart (es.Parts[1]);
+							if (newEnemies != null) {
+								foreach (EnemyShip newEnemyShip in newEnemies) {
+									enemyShips.Add (newEnemyShip);
+									Add (newEnemyShip);
+								}
+
+								newEnemies [0].direction = -1;
+								newEnemies [1].direction = 1;
+
+								enemyShips.Remove (es);
+								es.Kill ();						
+							}
+						}
+					}
+				}
+				enemyShips.RemoveAll ((EnemyShip s)=> {return s.Parent == null;});
+			}
+		}
+
+		private Vector2 GetMousePositionWorld ()
 		{
 			Vector2 mousePosition = new Vector2 (Mouse.GetState().X, Mouse.GetState ().Y);
 
-			return camera.GetUpperLeftPosition() + mousePosition; 
+			return camera.GetUpperLeftPosition () + mousePosition; 
 		}
 	}
 }
